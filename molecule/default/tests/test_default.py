d99973a666005e12dd86c3d3bbbcb00395f2a8ae
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-05-13 13:56:13 -0400

    Modify tests to check when the system was last upgraded

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -15,20 +15,32 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_debian_packages(host, pkg):
     """Test that appropriate packages were installed on Debian."""
     ansible_vars = host.ansible.get_variables()
-    if ansible_vars["inventory_hostname"] in ansible_vars['groups']['debian']:
+    if ansible_vars["inventory_hostname"] in ansible_vars["groups"]["debian"]:
         assert host.package(pkg).is_installed
 
 
-def test_debian_upgraded(host):
-    """Test that Debian instances were upgraded."""
+def test_debian_updated(host):
+    """Test that Debian instances were updated."""
     ansible_vars = host.ansible.get_variables()
-    if ansible_vars["inventory_hostname"] in ansible_vars['groups']['debian']:
-        assert (datetime.datetime.now() - host.file("/var/cache/apt").mtime).total_seconds() <= 24 * 60 * 60
+    if ansible_vars["inventory_hostname"] in ansible_vars["groups"]["debian"]:
+        print(host.file("/var/lib/apt/lists").mtime)
+        # Make sure that the instance was updated in the last 10
+        # minutes
+        assert (
+            datetime.datetime.now() - host.file("/var/lib/apt/lists").mtime
+        ).total_seconds() <= 10 * 60
 
 
-def test_redhat_upgraded(host):
-    """Test that RedHat instances were upgraded."""
+def test_redhat_updated(host):
+    """Test that RedHat instances were updated."""
     ansible_vars = host.ansible.get_variables()
-    if ansible_vars["inventory_hostname"] in ansible_vars['groups']['amazon']:
-        last_installation = datetime.datetime.strptime(host.run("rpm -qa --queryformat '%{installtime} %{installtime:date}\n' | sort --numeric-sort | tail --lines=1 | cut --delimiter=' ' --fields=2-").stdout, "%a %b %d %H:%M:%S %Y")
-        assert (datetime.datetime.now() - last_installation).total_seconds() <= 24 * 60 * 60
+    if ansible_vars["inventory_hostname"] in ansible_vars["groups"]["amazon"]:
+        last_update = datetime.datetime.strptime(
+            host.run(
+                "yum --quiet history list | cut --delimiter='|' --fields=3-4 | grep -F Update | cut --delimiter='|' --fields=1"
+            ).stdout.strip(),
+            "%Y-%m-%d %H:%M",
+        )
+        # Make sure that the instance was updated in the last 10
+        # minutes
+        assert (datetime.datetime.now() - last_update).total_seconds() <= 10 * 60

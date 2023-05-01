f8f8ad66bef03ff00657ca36aaa8162fb7c44c62
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2023-05-01 10:31:48 -0400

    Modify Amazon Linux tests to use DNF
    Amazon Linux 2023 uses dnf, not yum.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -32,33 +32,9 @@ def test_apt_updated(host):
 
 # This test can fail if there were no updates to install.
 @pytest.mark.xfail
-def test_yum_updated_time(host):
-    """Test that yum-based instances were updated."""
-    if host.system_info.distribution in ["amzn"]:
-        last_update = datetime.datetime.strptime(
-            host.run(
-                "yum --quiet history list | cut --delimiter='|' --fields=3-4 | grep --fixed-strings Update | cut --delimiter='|' --fields=1 | head --lines=1"
-            ).stdout.strip(),
-            "%Y-%m-%d %H:%M",
-        )
-        # Make sure that the instance was updated in the last 20
-        # minutes.
-        assert (datetime.datetime.now() - last_update).total_seconds() <= 20 * 60
-
-
-def test_yum_updated_command_output(host):
-    """Test that yum-based instances were updated."""
-    if host.system_info.distribution in ["amzn"]:
-        yum_output = host.run("yum update")
-        # There should be nothing to update.
-        assert "No packages marked for update" in yum_output.stdout
-
-
-# This test can fail if there were no updates to install.
-@pytest.mark.xfail
 def test_dnf_updated_time(host):
     """Test that dnf-based instances were updated."""
-    if host.system_info.distribution in ["fedora"]:
+    if host.system_info.distribution in ["amzn", "fedora"]:
         last_update = datetime.datetime.strptime(
             host.run(
                 "yum --quiet history list | cut --delimiter='|' --fields=3-4 | grep --fixed-strings U | cut --delimiter='|' --fields=1 | head --lines=1"
@@ -72,7 +48,7 @@ def test_dnf_updated_time(host):
 
 def test_dnf_updated_command_output(host):
     """Test that dnf-based instances were updated."""
-    if host.system_info.distribution in ["fedora"]:
+    if host.system_info.distribution in ["amzn", "fedora"]:
         dnf_output = host.run("dnf update")
         # There should be nothing to update.
         assert "Nothing to do." in dnf_output.stdout
